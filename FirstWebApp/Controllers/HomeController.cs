b5739741fea488c1ebba7c100d018ded5c89f87d
Hümbət Camalov <143912059+Camalzadeh@users.noreply.github.com>
2024-07-07 06:05:09 +0400

    finish app
    I write as same as mentors code, and all are working succesfully

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBD.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            /*ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "Index";*/
+            /*return Json(new
+            {
+                name="Ays"
+            });*/
+
+           /* return NoContent();*/
+
+            return View();
+        }
+    }
+}
